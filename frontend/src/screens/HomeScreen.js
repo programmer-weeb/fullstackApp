54a49faca13181a45651486675b9c08a87b31170
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2023-05-30 06:32:13 +0200

    Rename the Brand

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -45,7 +45,7 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>Amazona</title>
+        <title>Our Store</title>
       </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
